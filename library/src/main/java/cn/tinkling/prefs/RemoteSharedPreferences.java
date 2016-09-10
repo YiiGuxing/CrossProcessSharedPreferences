b62e71bc7b48d9d79b7fe8263499dc6c7d99b238
Tink-ling <tink-ling@foxmail.com>
2016-09-11 00:40:33 +0800

    去除测试代码

--- library/src/main/java/cn/tinkling/prefs/RemoteSharedPreferences.java
+++ library/src/main/java/cn/tinkling/prefs/RemoteSharedPreferences.java
@@ -79,11 +79,6 @@ public class RemoteSharedPreferences extends IRemoteSharedPreferences.Stub {
 
     @Override
     public int getInt(String key, int defValue) throws RemoteException {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         return mSharedPreferences.getInt(key, defValue);
     }
 
